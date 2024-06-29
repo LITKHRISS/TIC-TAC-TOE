bb31c268c2b276d2844f02abf5ca6038bef05a42
LITKHRISS <mordangmack@proton.me>
2024-06-29 19:11:32 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ const TURNS = {
   O: 'o'
 }
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''} `
+
+  const handleClick = () => {
+    updateBoard()
+  }
+
   return (
-    <div className="square">
+    <div onClick= {handleClick} className= {className}>
       {children}
     </div>
   )
@@ -17,6 +23,11 @@ function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () => {
+
+  }
 
   return (
     <main className='board'>
@@ -28,6 +39,7 @@ function App() {
             <Square
             key={index}
             index={index}
+            updateBoard={updateBoard}
             >
               {board[index]}
             </Square>
@@ -36,6 +48,14 @@ function App() {
       }
     </section>
 
+    <section className="turn">
+      <Square isSelected={turn === TURNS.X}>
+        {TURNS.X}
+      </Square>
+      <Square isSelected={turn === TURNS.O}>
+        {TURNS.O}
+      </Square>
+    </section>
   </main>
   ) 
 }
