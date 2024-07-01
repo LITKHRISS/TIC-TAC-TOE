cb11c7138fe26aaa9fcd9798292cb8a3bc692d69
LITKHRISS <mordangmack@proton.me>
2024-06-30 20:43:56 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''} `
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   }
 
   return (
@@ -25,8 +25,13 @@ function App() {
   )
   const [turn, setTurn] = useState(TURNS.X)
 
-  const updateBoard = () => {
+  const updateBoard = (index) => {
+    const newBoard = [ ... board]
+    newBoard[index] = turn
+    setBoard(newBoard)
 
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
   }
 
   return (
