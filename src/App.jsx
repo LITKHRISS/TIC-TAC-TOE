6ceb6d38d3b748702c69fc06cd0c99cccbf93a78
LITKHRISS <mordangmack@proton.me>
2024-07-06 18:05:38 -0400

    local storage updates

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,16 @@ function App() {
     console.log('render')
 
     const [board, setBoard] = useState(() => {
-      console.log('inicializar estado')
       const boardFromStorage = window.localStorage.getItem('board')
       if (boardFromStorage) return JSON.parse(boardFromStorage)
         return Array(9).fill(null)
     })
   
-    
-  const [turn, setTurn] = useState(TURNS.X)
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   // null means no winner, false means tie
   const [winner, setWinner] = useState(null) 
 
@@ -26,6 +28,10 @@ function App() {
       setBoard(Array(9).fill(null))
       setTurn(TURNS.X)
       setWinner(null)
+
+      window.localStorage.removeItem('board')
+      window.localStorage.removeItem('turn')
+      
     }
 
   const updateBoard = (index) => {
